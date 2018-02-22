19d42d957c8307ace4208a447d47b2f24dc16393
Kelsey Pintens <ksepintens@gmail.com>
2018-02-22 16:09:03 -0600

    deleted

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
 "use strict";
-
-let XHRcalls = require("./_XHRcalls.js");
-let outputCategory = require("./_output-category.js");
-let eventListeners = require("./_eventListener.js");
-
-let copyrightDate = require("./_copyrightDate.js");
-
-//Outputs current date and time to copyright area
-copyrightDate.output();
