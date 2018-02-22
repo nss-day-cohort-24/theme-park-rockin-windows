b020f9fc36e5f07011391b1679a872d54e8060f7
Amber Sharpe <amberjsharpe@gmail.com>
2018-02-22 16:25:28 -0600

    fixed button functionality

--- js/_output-category.js
+++ js/_output-category.js
@@ -1,17 +1,32 @@
 "use strict";
 
 var displayOutput = document.getElementById("display-output");
+
 let displayArea = (displayInput)  => {
     displayOutput.innerHTML = "";
-    if (displayInput["0"].area_id == "1") {
-    displayOutput.innerHTML += `<h1>Main Street U.S.A</h1>`;
-    console.log("It works");
+    let arrayOfAttractions = Object.values(displayInput);
+    console.log("displayInput", arrayOfAttractions[0]);
+    if (arrayOfAttractions[0].area_id == 1) {
+        console.log("button 1");
+        displayOutput.innerHTML += `<h1>Main Street U.S.A</h1>`;
+    } else if (arrayOfAttractions[0].area_id == 2) {
+        console.log("button 2");
+        displayOutput.innerHTML += `<h1>Adventureland</h1>`;   
+    } else if (arrayOfAttractions[0].area_id == 3) {
+        console.log("button 3");
+        displayOutput.innerHTML += `<h1>Frontierland</h1>`;
+    } else if (arrayOfAttractions[0].area_id == 4) {
+        console.log("button 4");
+        displayOutput.innerHTML += `<h1>Liberty Square</h1>`;
     }
-    console.log("output");
-    for (let item in displayInput) {
-        let areaPOI = displayInput[item];
+    console.log("outside if");
+    for (let item in arrayOfAttractions) {
+        let areaPOI = arrayOfAttractions[item];
         displayOutput.innerHTML += `<a href="#"><h2>${areaPOI.name}</h2></a>`;
     }
+    
 };
+
+
 
 module.exports = {displayArea, displayOutput};
