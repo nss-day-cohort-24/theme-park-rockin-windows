b61f3f11a2ca3c18fdce5ea847c09c016d4a71d8
lapinell <lapinell@gmail.com>
2018-02-22 11:37:51 -0600

    added current date update function

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@
 let XHRcalls = require("./_XHRcalls.js");
 let outputCategory = require("./_output-category.js");
 
+let copyrightDate = require("./_copyrightDate.js");
 
+//Outputs current date and time to copyright area
+copyrightDate.output();
