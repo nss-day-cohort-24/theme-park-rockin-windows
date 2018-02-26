3c3eb263dae789d3510793584781b5c4ded0a638
lapinell <lapinell@gmail.com>
2018-02-25 21:33:19 -0600

    sorting attractions by time success

--- js/_sortByTime.js
+++ js/_sortByTime.js
@@ -1,7 +1,8 @@
 "use strict";
 let moment = require('../lib/node_modules/moment/');
+let XHRcalls = require('../js/_XHRcalls.js');
 ///// Instructions
-// if they click 10 AM then insert 1 into let i
+// if they click 10  then insert 1 into let i
 
 // then we compare i to string and if true
 // then we return i
@@ -20,59 +21,24 @@ function getTime () {
 let userSelection = timeDropdown.addEventListener("change", getTime);
 console.log('user selection', userSelection);
 
-
-
-
 // full list of attraction
-let listOfAttractions = {
-"attractions": [
-    {
-        "id": 1,
-        "name": "Walt Disney World Railroad",
-        "description": "Listen to the clamor of the engine, the chug of the wheels and the call of the whistle as you travel aboard the Walt Disney World Railroad. Offering convenient stops at Frontierland, Fantasyland and Main Street, U.S.A., it's the perfect way to preview the magic at Magic Kingdom park and take in the picturesque scenery.",
-        "times" : ["8:00AM", "10:00AM"],
-        "type_id": 1,
-        "area_id": 1
-    },
-    {
-        "id": 2,
-        "name": "Main Street Vehicles",
-        "description": "Catch a nostalgic ride to Cinderella Castle or Town Square aboard a vintage vehicle. Board a classic horse-drawn car, omnibus, jitney or fire engine and sit back as you are ushered from one end of Main Street, U.S.A. to the other. Along the way, behold a bevy of 2- and 3-story-high, Victorian-themed shops and restaurants from a time and place long since passed.",
-        "times" : ["10:00AM", "11:40AM"],
-        "type_id": 1,
-        "area_id": 1
-    },
-    {
-        "id": 3,
-        "name": "Harmony Barber Shop",
-        "description": "Whether it’s your first haircut or your hundredth, the friendly and experienced barbers at Harmony Barber Shop on Main Street, U.S.A. will entertain you while making sure you look amazing. You may even be treated to the musical talents of the Dapper Dans, Disney’s very own barbershop quartet! And there’s a special My First Haircut  package for children that includes commemorative Mickey Ears and a certificate.",
-        "type_id": 4,
-        "area_id": 1
-    },
-    {
-        "id": 4,
-        "name": "Emporium",
-        "description": "The largest gift and souvenir shop in Magic Kingdom park, the Emporium offers apparel, plush toys, pins, candy and more. ",
-        "times" : ["9:00AM", "11:00AM"],
-        "type_id": 4,
-        "area_id": 1
-    }
-]
-};
+let listOfAttractions = XHRcalls.getTimeData();
 
-let attractionItem0 = listOfAttractions.attractions[3]; // output the object with the id of 4/name Emporium
+console.log('list of attractions', listOfAttractions);
+
+let attractionItem0 = listOfAttractions.attractions[3]; // output the object with the id of 4/ne Emporium
 // console.log('attraction item 3:', attractionItem0);
 
 let attractionIDValue = listOfAttractions.attractions[3].id; // outputs 4;
 // console.log('attraction 4 id number:', attractionIDValue);
 
-let attractionTimeValues = listOfAttractions.attractions[3].times; // outputs ["9:00AM", "11:00AM"];
+let attractionTimeValues = listOfAttractions.attractions[3].times; // outputs ["9:00", "11:00"];
 // console.log('attraction 4 Time Values:', attractionTimeValues);
 
 let attractionsWithTimes = []; 
 // console.log('attractionsWithTimes:', attractionsWithTimes);
 // we have an object - we can loop through the attraction array to check each item(object) for a key called times
-let checkForTimes = (A) => { //declare a fat arrow function with parameter A to pass in arrays
+let checkForTimes = (A) => { //declare a fat arrow function with pareter A to pass in arrays
     for (let i = 0; i < A.length; i++) {
         // console.log(A[i]); //outputs to console the current item-object
         // if the attractionItem has the key "times" then return the value of the id of that object item into the empty array attractionsWithTimes
@@ -113,13 +79,13 @@ let fillAttractionTimes = (A) => {
             if (attractionsWithTimes[i] === A[j].id) {
                 // console.log('attractionsWithTimes: ' + attractionsWithTimes[i] + '. Array Id: ' + A[j].id);
                 let timesArray = A[j].times; //store the items times in a local array
-                console.log('the id is ' + A[j].id + ' What times do I have? ', timesArray);
+                // console.log('the id is ' + A[j].id + ' What times do I have? ', timesArray);
                 // check the times property for any times that are not already stored in the attractionTimes array - if they are not, store them
                 
                 for(let k = 0; k < timesArray.length; k++) {
 
                     if ( attractionTimes.includes(timesArray[k]) === false) {
-                        console.log("time to add:", timesArray[k]);
+                        // console.log("time to add:", timesArray[k]);
                         attractionTimes.push(timesArray[k]);
                     }
                 }
@@ -139,50 +105,126 @@ console.log(moment().minute());
 
 let attractionTimesMilitary = [];
 
-for (let i = 0; i<attractionTimes.length; i++){
-    let validTime = moment(attractionTimes[i]).isValid();
+let convertTimeToMilitary = (A, B) => {
+    for (let i = 0; i<A.length; i++){
 
-    console.log('is valid? ', validTime);
+        let newTime = moment(A[i], "hh:mmaa").format("HH:mm");
 
-    let newTime = moment(attractionTimes[i], "hh:mmaa").format("HH:mm");
+        // console.log("military time to add", newTime);
 
-    console.log("military time to add", newTime);
+        B.push(newTime);
+    }
+};
 
-    attractionTimesMilitary.push(newTime);
-}
+// for (let i = 0; i<attractionTimes.length; i++){
+
+//     let newTime = moment(attractionTimes[i], "hh:mmaa").format("HH:mm");
+
+//     // console.log("military time to add", newTime);
+
+//     attractionTimesMilitary.push(newTime);
+// }
+
+convertTimeToMilitary(attractionTimes, attractionTimesMilitary);
 
 console.log('parsed attractionTimesMilitary array: ', attractionTimesMilitary);
+// // final result: attractionTimesMilitary = ["08:00", "10:00", "11:40", "09:00", "11:00"];
+
+//take off mm of military times
+let ABTkeys = [];
+
+let convertToHoursOnly = (A) => {
+    for (let i=0; i < A.length; i++) {
+        // console.log('time to convert:', A[i]);
+        //take first item in array
+        //take the mm off the item
+        let convertedTime = A[i].slice(0, 2);
+
+        // console.log('time converted:', convertedTime);
+
+        //if the item does not match any other items in the array 
+        for(let k = 0; k < A.length; k++) {
+
+            if ( ABTkeys.includes(convertedTime) === false) {
+                // console.log("time to add:", timesArray[k]);
+                ABTkeys.push(convertedTime);
+                // console.log('ABTkeys: ', ABTkeys);
+            }
+        //return item to new array called ABTkeys
+        }   
+    }
+};
+
+convertToHoursOnly(attractionTimesMilitary);
 
 
+// define properties and values of sortedAttractionsByTime object
+let sortedAttractionsByTime = {};
 
-// let attractionsByTime = {};
-// // create function that take the attractionTimes item values and make them into the attractionsByTime keys
+let convertItemsToKeys = (A) => { //parameter A is an array we want to convert to keys
+    console.log('converted hours', A);
+    for (let i = 0; i < A.length; i++){
+        let key = A[i]; //set variable key to current item in ABTkeys array
+        // console.log('current key is:', key);
+        sortedAttractionsByTime[key] = []; //set the key equal to an empty array
+        // console.log('current key value is:', sortedAttractionsByTime[key]);
+    }
+};
 
-// // Process:
-// attractionsByTime."8:00AM" //undefined
-// attractionsByTime."10:00AM" //undefine 
+convertItemsToKeys(ABTkeys);
 
-// // Result:
-// attractionsByTime = {
-//     "8:00AM" : undefined,
-//     "9:00AM" : undefined,
-//     "10:00AM" : undefined,
-//     "11:00AM" : undefined,
-//     "11:40AM" : undefined
-// };
+console.log('value of item 0 in sABT obj:', sortedAttractionsByTime[10]); // returns an empty array; to push to array => array.push('content') => sortedAttractionsByTime[10].push('attraction id')
 
-// // create a function that loops through the attractionsByTime object, 
-// //comparing the keys to the "times" array items in each of the attractionsWithTimes array
-// //if the key matches an item in the "times" array
-// //then store the id as a value of that key
+console.log('sort:', sortedAttractionsByTime); 
+//find all ids whose times property contains that hour
+
+for (let i = 0; i < attractionsWithTimes.length ; i++) {
+    let itemID = attractionsWithTimes[i]; //returns the id of the attraction
+    console.log('item ID is:', itemID);
+    
+    for(let j = 0; j < listOfAttractions.attractions.length; j++) { //loops through the list of attractions 
+        let currentAttractionsItem = listOfAttractions.attractions[j]; // sets the current attractions as a value of this variable
+        // console.log('current Attractions item: ', currentAttractionsItem);
+        if (itemID === currentAttractionsItem.id) { //if the itemID and id of the current attractions match
+            console.log('they match');
+            let timesOfItem = currentAttractionsItem.times; //then return the times of that current attraction
+            // console.log('Array of times for this item are: '+ timesOfItem);
+            
+            //convert times to military in preparation to compare
+            let militaryTimesOfItem = [];
+            convertTimeToMilitary(timesOfItem, militaryTimesOfItem); //convert the current attraction times to military times
+            console.log('militaryTimesOfItem:', militaryTimesOfItem);
+
+            for (let k = 0; k < militaryTimesOfItem.length; k++ ) { //loop through the converted times
+            let compareItem = militaryTimesOfItem[k]; //k
+            // console.log('comparison item', compareItem);
+            
+            for(let l = 0; l < ABTkeys.length; l++) {//then loop through the keys
+                let compareKey = ABTkeys[l]; // l
+                // console.log('ABT key 0:', compareKey);
+
+                if (compareItem.slice(0,2) === compareKey) { //if the converted time matches a key 
+                    //push the itemID to the sortedAttractionsByTime.0800
+                    // console.log("key " + compareKey + " to return itemID to:" + sortedAttractionsByTime[compareKey]);
+                    // console.log("id to push to key: ", itemID);
+                    sortedAttractionsByTime[compareKey].push(itemID); //then push the id of the attraction to that key
+                    console.log(sortedAttractionsByTime);
+                    } else {
+                    console.log('comparison failed');
+                }
+            }    
+        }
+    }
+}
+}
 
 //  //final result: 
-//  let attractionsByTime = {
-//      "8:00AM" : [1],
-//      "9:00AM" : [4],
-//      "10:00AM" : [1, 2],
-//      "11:00AM" : [4],
-//      "11:40AM" : [2]
+//  let sortedAttractionsByTime = {
+//      "08:00" : [1],
+//      "09:00" : [4],
+//      "10:00" : [1, 2],
+//      "11:00" : [4],
+//      "11:40" : [2]
 //  }; 
 
 // // //comparing userSelection to the attractionTimes
--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 "use strict";
 
+console.log('main.js starts');
+
 let XHRcalls = require("./_XHRcalls.js");
 let outputCategory = require("./_output-category.js");
 let eventListeners = require("./_eventListener.js");
 let sortTime = require("./_sortByTime.js");
 let moment = require('../lib/node_modules/moment');
 
-console.log('moment js begins');
 require('../lib/node_modules/moment/locale/cs');
-console.log(moment().hour());
-console.log('moment js ends');
+
+
 
 let copyrightDate = require("./_copyrightDate.js");
 
 //Outputs current date and time to copyright area
 copyrightDate.output();
-console.log('main.js loaded');
+console.log('main.js ends');
